212e9e829da22e25b41ea48a1ca3e0b856257b99
Ali Maktabi <maktabi876@gmail.com>
2024-12-11 12:54:40 +0330

    fixed responsive header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,6 +8,7 @@ import { SiGitbook } from "react-icons/si"
 import { PiMediumLogoFill } from "react-icons/pi"
 import { BsMenuButtonFill, BsTwitterX } from "react-icons/bs"
 import { useState } from "react"
+import { cn } from "@nextui-org/react"
 
 export function HeaderLink({ href, title }: { href: string; title: string }) {
   return <Link href={href}>{title}</Link>
@@ -17,47 +18,80 @@ export default function Header() {
   const [isHeaderOpen, setIsHeaderOpen] = useState(false)
 
   return (
-    <header className="h-20 border-b md:absolute static top-0 left-0 right-0 text-sm backdrop-blur-sm border-[#847D7D4D] px-3 lg:px-20 flex items-center gap-10">
-      <Link href="/">
-        <Image src="/imgs/logo.svg" alt="Symmio" width={170} height={40} />
-      </Link>
-
-      <div className="block ml-auto lg:hidden">
-        <FaBars size={24} />
-      </div>
-
-      <div className="md:flex items-center hidden gap-5 text-body-text">
-        <HeaderLink href="/symmio" title="Build on Symmio" />
-        <HeaderLink href="/symmio" title="Hedger" />
-        <HeaderLink href="/symmio" title="Protocol" />
-        <HeaderLink href="/symmio" title="Points" />
-      </div>
-
-      <div className="ml-0 lg:ml-auto hidden lg:flex items-center gap-10">
-        <Link href="#" className="flex gap-2 items-center">
-          <span>White Paper</span>
-          <FaRegFileLines />
+    <>
+      <header className="h-20 border-b md:absolute static top-0 left-0 right-0 text-sm backdrop-blur-sm border-[#847D7D4D] px-3 lg:px-20 flex items-center gap-10">
+        <Link href="/">
+          <Image src="/imgs/logo.svg" alt="Symmio" width={170} height={40} />
         </Link>
-        <Link href="#" className="flex text-primary gap-2 items-center">
-          <span>Trade</span>
-          <GoArrowUpRight size={20} className="-mb-1" />
-        </Link>
-      </div>
 
-      <div className="ml-14 text-gray100 hidden lg:flex items-center gap-8">
+        <div className="block ml-auto lg:hidden">
+          <FaBars onClick={() => setIsHeaderOpen(!isHeaderOpen)} size={24} />
+        </div>
+
+        <div className="md:flex items-center hidden gap-5 text-body-text">
+          <HeaderLink href="/symmio" title="Build on Symmio" />
+          <HeaderLink href="/symmio" title="Hedger" />
+          <HeaderLink href="/symmio" title="Protocol" />
+          <HeaderLink href="/symmio" title="Points" />
+        </div>
+
+        <div className="ml-0 lg:ml-auto hidden lg:flex items-center gap-10">
+          <Link href="#" className="flex gap-2 items-center">
+            <span>White Paper</span>
+            <FaRegFileLines />
+          </Link>
+          <Link href="#" className="flex text-primary gap-2 items-center">
+            <span>Trade</span>
+            <GoArrowUpRight size={20} className="-mb-1" />
+          </Link>
+        </div>
+
+        <div className="ml-14 text-gray100 hidden lg:flex items-center gap-8">
+          <Link href="#">
+            <SiGitbook />
+          </Link>
+          <Link href="#">
+            <PiMediumLogoFill size={20} />
+          </Link>
+          <Link href="#">
+            <BsTwitterX size={18} />
+          </Link>
+          <Link href="#">
+            <FaDiscord size={20} />
+          </Link>
+        </div>
+      </header>
+      <div
+        className={cn(
+          "fixed bottom-0 flex items-center justify-center gap-8 flex-wrap transition-all left-0 right-0 top-20 bg-gray00/90 z-10",
+          isHeaderOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none",
+        )}
+      >
         <Link href="#">
           <SiGitbook />
         </Link>
         <Link href="#">
-          <PiMediumLogoFill size={20} />
+          <PiMediumLogoFill size={22} />
         </Link>
         <Link href="#">
-          <BsTwitterX size={18} />
+          <BsTwitterX size={20} />
         </Link>
         <Link href="#">
-          <FaDiscord size={20} />
+          <FaDiscord size={22} />
         </Link>
+        <div className="flex gap-4 items-center">
+          <Link href="#" className="flex gap-2 items-center">
+            <span>White Paper</span>
+            <FaRegFileLines />
+          </Link>
+          <Link href="#" className="flex text-primary gap-2 items-center">
+            <span>Trade</span>
+            <GoArrowUpRight size={20} className="-mb-1" />
+          </Link>
+        </div>
       </div>
-    </header>
+    </>
   )
 }
